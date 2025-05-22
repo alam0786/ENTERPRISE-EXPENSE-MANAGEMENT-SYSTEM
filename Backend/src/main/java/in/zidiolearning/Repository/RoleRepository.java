878371073fc236ec5d2381fe184cbde1c22273bb
Md Arshad <arshadmd080@gmail.com>
2025-05-22 12:37:26 +0530

    Create repository class for User & Role

---
+++ Backend/src/main/java/in/zidiolearning/Repository/RoleRepository.java
@@ -0,0 +1,16 @@
+package in.zidiolearning.Repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import in.zidiolearning.Entity.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long>
+{
+
+	Optional<Role> findByName(String name);
+	
+}
